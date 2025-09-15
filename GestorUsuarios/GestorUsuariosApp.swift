71e7f2ec3a56637a3e3f378a6b4b9810a41379bf
Patricio Chavez <patricio.alejandroch@hotmail.com>
2025-09-15 11:45:31 -0500

    Project setup

---
+++ GestorUsuarios/GestorUsuariosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestorUsuariosApp.swift
+//  GestorUsuarios
+//
+//  Created by Patricio Chavez on 15/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GestorUsuariosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GestorUsuariosTests/GestorUsuariosTests.swift
@@ -0,0 +1,17 @@
+//
+//  GestorUsuariosTests.swift
+//  GestorUsuariosTests
+//
+//  Created by Patricio Chavez on 15/9/25.
+//
+
+import Testing
+@testable import GestorUsuarios
+
+struct GestorUsuariosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
